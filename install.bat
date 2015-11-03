10e270f1428240ab07d5a0137c4f151aadc1ad56
Giorgos Korfiatis <gkorf@grnet.gr>
2015-11-03 16:09:11 +0200

    Change cygwin mirror

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
 if not exist "%~dp0\build\cygwin" mkdir %~dp0\build\cygwin
 if not exist "%~dp0\build\cygwin\packages" mkdir %~dp0\build\cygwin\packages
-.\setup-x86_64.exe -q  -N -n -d -R %~dp0\build\cygwin -l %~dp0\build\cygwin\packages -s ftp://ftp.gabrys.biz/pub/cygwin -P python,gnupg,librsync-devel,librsync1,wget,python-setuptools,gcc-core,gcc
+.\setup-x86_64.exe -q  -N -n -d -R %~dp0\build\cygwin -l %~dp0\build\cygwin\packages -s ftp://gd.tuwien.ac.at/gnu/cygwin/ -P python,gnupg,librsync-devel,librsync1,wget,python-setuptools,gcc-core,gcc
 %~dp0\build\cygwin\bin\bash.exe %~dp0\install_duplicity.sh
