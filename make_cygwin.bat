8e390f6666709e1c5e3c64c8a95362a2cb63245b
Giorgos Korfiatis <gkorf@grnet.gr>
2015-11-11 14:40:56 +0200

    Make build and dist instances of cygwin

--- make_cygwin.bat
+++ make_cygwin.bat
@@ -1,3 +1,24 @@
-if not exist "%~dp0\build\cygwin" mkdir %~dp0\build\cygwin
-if not exist "%~dp0\build\cygwin\packages" mkdir %~dp0\build\cygwin\packages
-.\setup-x86_64.exe -q -Y -N -n -d -R %~dp0\build\cygwin -l %~dp0\build\cygwin\packages -s ftp://ftp.snt.utwente.nl/pub/software/cygwin/ -P python,gnupg,librsync-devel,librsync1,wget,python-setuptools,gcc-core,gcc,patch,unzip
+@echo off
+set MIRROR=ftp://ftp.snt.utwente.nl/pub/software/cygwin/
+set PACKAGES=python,gnupg,librsync1,python-setuptools
+set NAME=cygwin
+
+if "%1" == "build" (
+set NAME=%NAME%_build
+set PACKAGES=%PACKAGES%,librsync-devel,wget,gcc-core,gcc,patch,unzip
+) else (
+if not "%1" == "dist" (
+@echo Must give option build or dist
+goto end
+))
+
+set BUILD=%~dp0%build
+set CYGWIN=%BUILD%\%NAME%
+set CACHE=%BUILD%\cygwin_packages
+@echo %CYGWIN%
+@echo %CACHE%
+@echo %PACKAGES%
+if not exist %CYGWIN% mkdir %CYGWIN%
+if not exist %CACHE% mkdir %CACHE%
+.\setup-x86_64.exe -q -Y -N -n -d -R %CYGWIN% -l %CACHE% -s %MIRROR% -P %PACKAGES%
+:end
