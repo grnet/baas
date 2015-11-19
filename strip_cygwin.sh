340888867100109a229530f535ec1d333a2ee398
Giorgos Korfiatis <gkorf@grnet.gr>
2015-11-19 11:30:42 +0200

    Don't remove groff

--- strip_cygwin.sh
+++ strip_cygwin.sh
@@ -22,7 +22,6 @@ cd $CYGWIN
 
 rm -r usr/share/doc # 26M
 find usr/share/locale/ -maxdepth 1 -mindepth 1 | grep -v en | xargs rm -r # 39M
-rm -r usr/share/groff # 5M
 rm -r usr/share/man/* # 3M
 rm -r usr/share/info/* # 2M
 cd bin && rm dumper.exe objdump.exe as.exe lynx.exe ld.exe ld.bfd.exe strip.exe objcopy.exe gprof.exe && cd ..
